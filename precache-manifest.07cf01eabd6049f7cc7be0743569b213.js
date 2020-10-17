0a59d432103c12810d2e26163e7aa393f741b408
kanwarpreet-singh <kanwarpreetsidana1200@gmail.com>
2020-10-17 21:22:59 +0530

    Updates

---
+++ precache-manifest.07cf01eabd6049f7cc7be0743569b213.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa62a97250b46c9d863afc6ad7c9d519",
+    "url": "/alpha/index.html"
+  },
+  {
+    "revision": "5a6f475c10c49bd20851",
+    "url": "/alpha/static/css/3.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "4a5dcce95d781267ef48",
+    "url": "/alpha/static/css/main.2deb4546.chunk.css"
+  },
+  {
+    "revision": "be397a881df53eaa8428",
+    "url": "/alpha/static/js/2.e7d81991.chunk.js"
+  },
+  {
+    "revision": "5a6f475c10c49bd20851",
+    "url": "/alpha/static/js/3.be8616b0.chunk.js"
+  },
+  {
+    "revision": "94cf1433cf1a1601b3291812927c11cf",
+    "url": "/alpha/static/js/3.be8616b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332c2dc28f9eabfb2841",
+    "url": "/alpha/static/js/4.ed6e4598.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/alpha/static/js/4.ed6e4598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482d2a947c45fdfc0787",
+    "url": "/alpha/static/js/5.1afa70ba.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/alpha/static/js/5.1afa70ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5dcce95d781267ef48",
+    "url": "/alpha/static/js/main.2730cdb8.chunk.js"
+  },
+  {
+    "revision": "291665e23795851cd26f",
+    "url": "/alpha/static/js/runtime-main.dfe83194.js"
+  },
+  {
+    "revision": "0f8a486e2e7d40e49ce12d236d833e62",
+    "url": "/alpha/static/media/abt_img.0f8a486e.jpg"
+  },
+  {
+    "revision": "14b230eb489bb2eba2685fec486ff8be",
+    "url": "/alpha/static/media/digital_mark.14b230eb.jpg"
+  },
+  {
+    "revision": "caf5e75b17bb3b6022f0ffd3d15dfceb",
+    "url": "/alpha/static/media/h1.caf5e75b.jpeg"
+  },
+  {
+    "revision": "88a9971ace07a5343a907aaa2f83b417",
+    "url": "/alpha/static/media/img1.88a9971a.png"
+  },
+  {
+    "revision": "b119dc3d5ac821c6e6246d6a33e73786",
+    "url": "/alpha/static/media/marketing.b119dc3d.jpg"
+  },
+  {
+    "revision": "3b2992312eb4b1c63a63c351dfaf3355",
+    "url": "/alpha/static/media/mobile_app.3b299231.png"
+  },
+  {
+    "revision": "7ebcce46e899613a7d9f374473890880",
+    "url": "/alpha/static/media/software.7ebcce46.png"
+  },
+  {
+    "revision": "9e37b5ab7e8835352e04b3cee6f81f06",
+    "url": "/alpha/static/media/softwaredevelopment.9e37b5ab.png"
+  }
+]);
